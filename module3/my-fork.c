f6f8f33479495b94059efb71d40d98a340d7eefa
Yanick Duchesne <yanickduchesne@gmail.com>
2022-11-19 18:22:35 -0500

    Fixed format

--- module3/my-fork.c
+++ module3/my-fork.c
@@ -10,12 +10,12 @@
 /**
  * A simple program illustrating fork/wait functionality for basic
  * multi-processing and process coordination:
- * 
+ *
  * - Parent forks a child process and waits on child completion.
  * - Child does some work and exits.
  * - Parent recuperates child's exit code, returns its own exit
  *   code accordingly.
- * 
+ *
  */
 
 int main() {
@@ -35,33 +35,30 @@ int main() {
   // occurring in the child process.
   case 0:
     while (loopCount < MAX_LOOPS) {
-      printf("Running child process (pid: %u, loopCount = %u)\n", getpid(),
-             loopCount);
+      printf("Child PID=%d. Running iteration #%u\n", getpid(), loopCount);
       sleep(SLEEP_INTERVAL);
       loopCount++;
     }
-    printf("Exiting child process (pid: %u, loopCount = %u)\n", getpid(),
-          loopCount);
+    printf("Exiting child PID=%d. Iteration #%u)\n", getpid(), loopCount);
     return EXIT_SUCCESS;
   // Otherwise, execution is in the context of the parent:
   // that process waits until the child completes and collects
   // the child's status.
   default:
     int childStatus;
-    printf("Parent waiting for completion of child process (parent pid = %u, "
-           "child pid = %u)\n",
-           getpid(), childPid);
+    printf("Parent PID=%d. Waiting (child PID=%d)\n", getpid(), childPid);
     waitpid(childPid, &childStatus, 0);
 
     printf("Child process exited, parent process is resuming execution (parent "
-           "pid = %u, "
-           "child pid = %u)\n",
+           "PID=%d, "
+           "child PID=%d)\n",
            getpid(), childPid);
     if (WIFSIGNALED(childStatus)) {
       printf("Child process exit code (%u) indicates failure\n", childStatus);
       return EXIT_FAILURE;
     }
-    printf("Child process exit code (%u) indicates successful completion\n", childStatus);
+    printf("Child process exit code (%u) indicates successful completion\n",
+           childStatus);
     return EXIT_SUCCESS;
   }
 }
